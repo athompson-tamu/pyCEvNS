c7fc379b3718e0fcdc187d228afc4553eebdb4b0
Adrian Thompson <31362409+athompson-tamu@users.noreply.github.com>
2019-08-27 17:49:06 -0500

    Update frank.R

--- copula/frank.R
+++ copula/frank.R
@@ -4,29 +4,29 @@ library(VineCopula)
 
 coh <- read.table("coherent_ud_post_equal_weights.dat", header=FALSE)
 
-#plot(coh[,1], 1-coh[,7])
+# Generate plots. We rotate the down-type NSI so that the correlation is positive.
+plot(coh[,1], -coh[,7])
+plot(coh[,2], -coh[,8])
+plot(coh[,3], -coh[,9])
+plot(coh[,4], -coh[,10])
+plot(coh[,5], -coh[,11])
+plot(coh[,6], -coh[,12])
 
-# Read in pseudo observations.
-z <- pobs(as.matrix(cbind(coh[,1],coh[,2],coh[,4],coh[,5],coh[,6],coh[,7],coh[,8],coh[,10],coh[,11],coh[,12])))
+# Read in pseudo observations. Again, flip the values for d-type so that the fit will work.
+z <- pobs(as.matrix(cbind(coh[,1],coh[,2],coh[,4],coh[,5],coh[,6],-coh[,7],-coh[,8],-coh[,10],-coh[,11],-coh[,12])))
 
 # Estimate copula parameters
 cop_model <- frankCopula(dim = 10)
-#m <- pobs(as.matrix(z))
 fit <- fitCopula(cop_model, z, method = 'itau')
 coef(fit)
 
 ##    param 
-## -6.647493
+## ~1.488821
 
 # Run goodness of fit.
-gf <- gofCopula(frankCopula(dim = 10, param=-1.469), z, N = 50, estim.method = "itau")
+gf <- gofCopula(frankCopula(dim = 10, param=1.488821), z, N = 50, estim.method = "itau")
 gf
 
 
-gf <- gofCopula(normalCopula(dim = 10), z, N = 50, method = "itau")
+gf <- gofCopula(normalCopula(dim = 10), z, N = 50, estim.method = "itau")
 gf
-
-## 	Parametric bootstrap goodness-of-fit test with 'method'="Sn", 'estim.method'="mpl"
-## 
-## data:  x
-## statistic = 0.29449, parameter = 0.59983, p-value = 0.009804
