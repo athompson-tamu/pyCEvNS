5524acdf749295ca7a316769c9753dd13c17b559
Adrian Thompson <31362409+athompson-tamu@users.noreply.github.com>
2020-03-05 16:22:39 -0600

    Added mev_per_hz

--- pyCEvNS/constants.py
+++ pyCEvNS/constants.py
@@ -8,6 +8,7 @@ hbar = 6.58212e-22  # MeV*s
 c_light = 2.998e8  # m/s
 meter_by_mev = hbar * c_light  # MeV*m
 mev_per_kg = 5.6095887e29  # MeV/kg
+mev_per_hz = 6.58e-22  # 1 s^-1 = 6.58e-22 MeV
 
 # SM parameters
 ssw = 0.2312  # Sine-squared of the Weinberg angle
@@ -32,3 +33,7 @@ massof3he = 5.008234e-24
 massof12c = 1.993e-23
 massof14n = 2.3252651e-23
 massof16o = 2.656018e-23
+
+## quark mass in MeV
+massofu = 2.2
+massofd = 4.7
